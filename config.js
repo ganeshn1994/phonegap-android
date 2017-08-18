beb9873448f6987fe745aafa9d51cc390e81c838
ganesh n <ganeshn@ganeshs-MacBook-Pro.local>
2017-08-19 01:26:51 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'test',
+		wp_ws_url : 'http://shtechculture.com/wp-appkit-api/test',
+		wp_url : 'http://shtechculture.com',
+		theme : 'q-android',
+		version : '1.0',
+		app_title : 'Test',
+		app_platform : 'ios',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'put your unique phrase here',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
